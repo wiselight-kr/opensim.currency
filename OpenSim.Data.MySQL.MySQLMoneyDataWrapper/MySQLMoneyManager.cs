60a09dbb193a504bee0aed446e57715f21f9f0f7
iseki <iseki@72fa41ff-ebb8-4f2e-bc52-925b2cbe95aa>
2020-05-26 09:58:19 +0000

    by UbitUmarov

--- OpenSim.Data.MySQL.MySQLMoneyDataWrapper/MySQLMoneyManager.cs
+++ OpenSim.Data.MySQL.MySQLMoneyDataWrapper/MySQLMoneyManager.cs
@@ -941,10 +941,15 @@ namespace OpenSim.Data.MySQL.MySQLMoneyDataWrapper
 						retValue = Convert.ToInt32(dbReader["balance"]);
 					}
 				}
+#pragma warning disable CS0168 // The variable 'e' is declared but never				
+#pragma warning disable CA1031 // Do not catch general exception types
 				catch (Exception e) {
 					m_log.ErrorFormat("[MoneyDB]: MySql failed to fetch balance {0}.", userID);
 					retValue = -2;
 				}
+#pragma warning restore CA1031 // Do not catch general exception types
+#pragma warning restore CS0168 // The variable 'e' is declared but never
+
 				dbReader.Close();
 			}
 			cmd.Dispose();
--- OpenSim.Grid.MoneyServer/IMoneyDBService.cs
+++ OpenSim.Grid.MoneyServer/IMoneyDBService.cs
@@ -18,23 +18,27 @@
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
  * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#pragma warning disable S1128 // Unused "using" should be removed
 using System;
 using System.Collections.Generic;
-//using System.Linq;
 using System.Text;
 using OpenMetaverse;
 using OpenSim.Data.MySQL.MySQLMoneyDataWrapper;
+#pragma warning restore S1128 // Unused "using" should be removed
 
 
 namespace OpenSim.Grid.MoneyServer
 {
+    /// <summary>
+    /// IMoney DB Service
+    /// </summary>
     public interface IMoneyDBService
     {
         int getBalance(string userID);
@@ -66,5 +70,6 @@ namespace OpenSim.Grid.MoneyServer
         bool TryAddUserInfo(UserInfo user);
 
         UserInfo FetchUserInfo(string userID);
+
     }
 }
